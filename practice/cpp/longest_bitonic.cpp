0de7e148b6b30b0eff5cd246d23178057fd704f8
Rahul Paul <paul.rahul@gmail.com>
2013-11-15 19:44:28 +0530

    Fixed some bugs and modified the problem.

--- practice/cpp/longest_bitonic.cpp
+++ practice/cpp/longest_bitonic.cpp
@@ -27,8 +27,10 @@ int main() {
 	while (t--) {
 		cin >> n;
 		vector<int> arr(n, 0);
-		vector<int> l(n, 0);
-		vector<int> r(n, 0);
+		vector<int> l(n, 1);
+		vector<int> r(n, 1);
+		vector<int> lp(n, -1);
+		vector<int> rp(n, -1);
 		
 		for (int i = 0; i < n; ++i) {
 			cin >> arr[i];
@@ -39,25 +41,58 @@ int main() {
 		
 		for (int i = 1; i < n; ++i) {
 			for (int j = 0; j < i; ++j) {
-				if (arr[j] < arr[i] && l[j] + 1 > l[i]) {
+				if (arr[j] <= arr[i] && l[j] + 1 > l[i]) {
 					l[i] = l[j] + 1;
+					lp[i] = j;
 				}
 			}
 		}
 		
 		for (int i = n - 2; i >= 0; --i) {
-			for (int j = n - 1; j > i; --j) {
-				if (arr[j] < arr[i] && r[j] + 1 > r[i]) {
+			for (int j = i + 1; j < n; ++j) {
+				if (arr[j] <= arr[i] && r[j] + 1 > r[i]) {
 					r[i] = r[j] + 1;
+					rp[i] = j;
 				}
 			}
-		}		
+		}
+		
+		// for (int i = 0; i < n; ++i) {
+		// 	cout << lp[i] << " ";
+		// }
+		// cout << endl;
+		// for (int i = 0; i < n; ++i) {
+		// 	cout << rp[i] << " ";
+		// }		
+		// cout << endl;
 		
 		int ans = 0;
+		int mx = 0;
 		for (int i = 0; i < n; ++i) {
-			ans = max(ans, l[i] + r[i] - 1);
+			int c = l[i] + r[i] - 1;
+			if (c > mx) {
+				mx = c;
+				ans = i;
+			}
 		}
-		cout << ans << endl;
+		// cout << ans << " " << mx << endl;
+		
+		int idx = ans;
+		vector<int> path;
+		while (idx >= 0) {
+			path.push_back(arr[idx]);
+			idx = lp[idx];
+		}
+		for (int i = path.size() - 1; i >= 0; --i) {
+			cout << path[i] << " ";
+		}
+		idx = rp[ans];
+		while (idx >= 0 && idx < n) {
+			cout << arr[idx] << " ";
+			idx = rp[idx];
+		}
+		
+		cout << endl;
 	}
 
 	return 0;
