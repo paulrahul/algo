5317f5d2b708037751761c08e596d481995d9d91
Rahul Paul <paul.rahul@gmail.com>
2013-11-18 20:20:17 +0530

    Hackerrank: Bday gift (bug fix and optimization)

--- hackerrank/bday_gift.py
+++ hackerrank/bday_gift.py
@@ -1,25 +1,7 @@
-def calc_num(arr, n):
-    lim = 2 ** n
-    ans = 0
-    for i in xrange(0, lim):
-        j = 0
-        x = i
-        s = 0
-        while x > 0:
-            if x & 1:
-                s += arr[j]
-            x >>= 1
-            j += 1
-        ans += s
-    return ans
-
 n = int(raw_input())
-arr = []
+num = 0
 for i in range(0, n):
     t = int(raw_input())
-    arr.append(t)
+    num += t
 
-num = calc_num(arr, n)
-den = 2 ** n
-#print num, den
-print "%.1f" % (num / float(den))
+print "%.1f" % (num / float(2))
