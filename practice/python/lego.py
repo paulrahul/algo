8641bc4063729e16b0fc7d2ebe1b6f568b33fff8
Rahul <paul.rahul@gmail.com>
2020-10-11 12:57:00 +0200

    Added some visualisation.

--- practice/python/lego.py
+++ practice/python/lego.py
@@ -1,9 +1,31 @@
+'''
+TODOs:
+1. Make the scale magnification easier.
+2. Add ability to treat Black as the default floor color.
+'''
+
 from collections import namedtuple
+import random
+
+from PIL import Image, ImageColor, ImageDraw
 
 edge = namedtuple("edge", "index type")
 block = namedtuple("block", "color start end")
+MAG = 13                                                                                            
+def solve(color_map, coord_map, y, im):
+    draw = ImageDraw.Draw(im)
+    draw.text((0, y + 5), "Seen from bottom")
 
-def solve(color_map, coord_map):
+    y += 20
+    solution = find_top_view(color_map, coord_map)
+    for color, start, end in solution:                          
+        #print "%s %d %d" % (color, start, end)
+        draw_rect(start, end, y, color, im)
+
+    draw.line((0, y + 20, 1300, y + 20), (128, 128, 128), 2)
+    return y + 40
+
+def find_top_view(color_map, coord_map):
     sorted_idx = coord_map.keys()
     sorted_idx.sort()
 
@@ -16,6 +38,8 @@ def solve(color_map, coord_map):
     prev_idx = coord_map[prev_coord].index
     new_color = color_map[prev_idx].color
     top_idx = prev_idx
+
+    solution = []
     for i in range(1, l):
         curr_coord = sorted_idx[i]
         curr_idx = coord_map[sorted_idx[i]].index
@@ -29,7 +53,7 @@ def solve(color_map, coord_map):
             color_map.pop(curr_idx)
             continue
 
-        print "%s %d %d" % (new_color, prev_coord, curr_coord)
+        solution.append((new_color, prev_coord, curr_coord))
         if curr_type == "L":
             top_idx = curr_idx
             new_color = color_map[curr_idx].color
@@ -49,24 +73,52 @@ def solve(color_map, coord_map):
                 new_color = "B"
 
         prev_coord = curr_coord
-        
+    
+    return solution
+
+def init_image():
+    return Image.new('RGBA', (1400, 768), (128, 128, 128, 0))
+
+def finalise_image(im):
+    im.save('pillow_imagedraw.png', quality=95)
+
+def draw_rect(start, end, y, color, im):
+    #print "draw %d %d %d %s" % (start, end, y, color)
+    start *= MAG
+    end *= MAG
+
+    draw = ImageDraw.Draw(im)
+    draw.rectangle((start, y, end, y + 10), fill=color,
+                    outline=(255, 255, 255))
+
 if __name__ == "__main__":
+    im = init_image()
+
+    colors = set()
     color_map = {}  # coord index to block element
     coord_map = {}  # coordinate to edge element
     index = 0
+    y = 10
     while True:
         try:
             inp = raw_input().split()
             l = len(inp)
             if l == 0:
-                solve(color_map, coord_map)
+                y = solve(color_map, coord_map, y, im)
 
                 index = 0
                 color_map.clear()
                 coord_map.clear()
                 continue
 
-            color = inp[0]
+            rand_key = ImageColor.colormap.keys()[random.randint(0, len(ImageColor.colormap) - 1)]
+            color = ImageColor.colormap[rand_key]
+            while color in colors:
+                rand_key = ImageColor.colormap.keys()[random.randint(0, len(ImageColor.colormap) - 1)]
+                color = ImageColor.colormap[rand_key]
+            if inp[0]:
+                color = ImageColor.getrgb(inp[0]) + tuple((128,))
+
             for i in range(0, l / 2):
                 start = int(inp[2 * i + 1])
                 end = int(inp[2 * i + 2])
@@ -74,8 +126,16 @@ if __name__ == "__main__":
                 coord_map[start] = edge(index, "L")
                 coord_map[end] = edge(index, "R")
 
+                draw_rect(start, end, y, color, im)
+
                 index += 1
+            y += 15
         except EOFError:
             break
 
-    solve(color_map, coord_map)
+    solve(color_map, coord_map, y, im)
+
+    #draw_rect(10, 20, 100, (0, 128, 0, 128), im)
+    #raw_rect(15, 30, 100, (0, 0, 255, 128), im)
+
+    finalise_image(im)
