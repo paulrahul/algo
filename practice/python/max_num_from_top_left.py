fad9a09b5bd996be69cc5d641a3150ba8a157a44
Rahul <paul.rahul@gmail.com>
2021-01-21 23:28:08 +0100

    Non-recursive solution but with slightly high runtime complexity. Timing out for huge inputs.
    R - O((M+N)^2), S - O(MN)
    
    Can be reduced to O((M+M)Mlgn). Maybe.

--- practice/python/max_num_from_top_left.py
+++ practice/python/max_num_from_top_left.py
@@ -2,81 +2,112 @@
 https://app.codility.com/programmers/task/max_path_from_the_left_top_corner/
 '''
 
-def find_prev_max(A, n, aux, idx1, idx2):
-    if idx1 == idx2:
-        return idx1 // n, idx1 % n
-
-    r1 = idx1 // n
-    c1 = idx1 % n
-
-    r2 = idx2 // n
-    c2 = idx2 % n
-
-    if A[r1][c1] == A[r2][c2]:
-        return find_prev_max(A, n, aux, aux[r1][c1], aux[r2][c2])
+def get_neigbour_score(aux_pos, m, n, r, c):
+    l = []
+    if r == m - 1 and c < n  - 1:
+        l.append(aux_pos[r][c+1])
+    elif r < m - 1 and c == n - 1:
+        l.append(aux_pos[r + 1][c])
     else:
-        if A[r1][c1] > A[r2][c2]:
-            return r1, c1
-        else:
-            return r2, c2
+        l.append(aux_pos[r][c + 1])
+        l.append(aux_pos[r + 1][c])
 
+    return l
+
+
+def get_min_neighbour(aux_pos, m, n, r, c):
+    return min(get_neigbour_score(aux_pos, m, n, r, c))
+
+def process_diagonal(A, m, n, r, c, aux_pos):
+    m = len(A)
+    n = len(A[0])
+
+    # The indices in consideration are:
+    # A[r][c], A[r - 1][c + 1], A[r - 2][c + 2]....A[0][n]
+    l = []
+    r1 = r
+    c1 = c
+    while r1 >= 0 and c1 < n:
+        l.append((A[r1][c1], r1 * n + c1))
+        r1 -= 1
+        c1 += 1
+
+    #print("column diagonal list for %d,%d: %s" % (r, c, l))
+
+    # Now sort (descending) l based on the value first, then neighbouring
+    # values.
+    llen = len(l)
+    for j in range(0, llen -1):
+        for k in range(j + 1, llen):
+            swap = (l[k][0] > l[j][0])
+            if l[k][0] == l[j][0]:
+                # Check neighbours.
+                n_pos1 = get_min_neighbour(
+                        aux_pos, m, n, l[k][1] // n, l[k][1] % n)
+                n_pos2 = get_min_neighbour(
+                        aux_pos, m, n, l[j][1] // n, l[j][1] % n)
+
+                swap = (n_pos1 < n_pos2)
+
+            if swap:
+                tmp = l[j]
+                l[j] = l[k]
+                l[k] = tmp
+
+    r1 = r
+    c1 = c
+    k = 0
+    while r1 >= 0 and c1 < n:
+        val = l[k][0]
+        orig_r = l[k][1] // n
+        orig_c = l[k][1] % n
+
+        aux_pos[orig_r][orig_c] = k
+
+        k += 1
+        r1 -= 1
+        c1 += 1
+
+    #print("column diagonal list for %d,%d: %s" % (r, c, l))
 
 def solution(A):
     m = len(A)
     n = len(A[0])
 
-    aux = [[-1 for x in range(n)] for y in range(m)]
+    # Position of each element in the matrix in the sorted list of its
+    # diagonal.
+    aux_pos = [[-1 for x in range(n)] for y in range(m)]
 
-    # Start from the bottom-right.
-    for i in range(m):
-        for j in range(n):
-            r = m - 1 - i
-            c = n - 1 - j
+    # Start from the bottom-right and take each diagonal.
+    for i in range(n):
+        c = n - 1 - i
+        process_diagonal(A, m, n, m - 1, c, aux_pos)
 
-            aux_r = -1
-            aux_c = -1
-            # Compare the two neighbours.
-            if r == m - 1 and c == n - 1:
-                aux_r = r
-                aux_c = c
-            elif r == m - 1 and c < n - 1:
-                aux_r = r
-                aux_c = c + 1
-            elif r < m - 1 and c == n - 1:
-                aux_r = r + 1
-                aux_c = c
-            else:
-                if A[r][c + 1] == A[r + 1][c]:
-                    aux_r, aux_c = find_prev_max(
-                        A, n, aux, aux[r][c + 1], aux[r + 1][c])
-                else:
-                    if A[r][c + 1] > A[r + 1][c]:
-                        aux_r = r
-                        aux_c = c + 1
-                    else:
-                        aux_r = r + 1
-                        aux_c = c
+    for i in range(m - 1):
+        r = m - 2 - i
+        process_diagonal(A, m, n, r, 0, aux_pos)
 
-            #print("changing %d %d to %d\n" % (r, c, aux_r*n+aux_c))
-            #aux[r][c] = aux[aux_r][aux_c]
-            aux[r][c] = aux_r * n + aux_c
-            #print(aux)
 
-    aux[m - 1][n - 1] = -1
+    #print(aux_pos)
 
-    # Construct the number by traversing aux from top-left to right-bottom.
-    r = c = 0
     ans = ""
+    r = 0
+    c = 0
     while True:
         ans += str(A[r][c])
 
-        idx = aux[r][c]
-        r = idx // n
-        c = idx % n
+        if r == m - 1 and c == n - 1:
+            break
 
-        if r < 0:
-            break        
-    print(aux)
+        if r == m - 1 and c < n - 1:
+            c += 1
+        elif r < m - 1 and c == n - 1:
+            r += 1
+        else:
+            if aux_pos[r][c + 1] < aux_pos[r + 1][c]:
+                c += 1
+            else:
+                r += 1
 
     return ans
 
@@ -113,4 +144,3 @@ if __name__ == "__main__":
             [9, 1, 2]
         ]
     ))
-
