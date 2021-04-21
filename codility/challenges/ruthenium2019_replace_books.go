3675e5dd384175d8bf4b51238710b075c6f9500c
paulrahul <paul.rahul@gmail.com>
2021-04-21 22:42:36 +0200

    Latest solution. 53% correct.

--- codility/challenges/ruthenium2019_replace_books.go
+++ codility/challenges/ruthenium2019_replace_books.go
@@ -1,3 +1,11 @@
+/*
+Solution for https://app.codility.com/programmers/challenges/ruthenium2019/
+
+R - O(N^3)
+S - O(N^2)
+
+*/
+
 package main
 
 import (
@@ -11,83 +19,81 @@ func max(x, y int) int {
 	return y
 }
 
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-type RangeAns struct {
-	ans      int
-	numKUsed int
-}
-
 func Solution(arr []int, k int) int {
 	n := len(arr)
 
 	numIndices := make(map[int][]int)
-	for i, num := range arr {
+	for i, num := range arr { // O(N)
 		numIndices[num] = append(numIndices[num], i)
 	}
 
+	// Now find the best solution for each unique number in the original array.
+	// i.e. for each number, what is the max. continous sequence that we can
+	// get with at most k replacements. We will use a greedy approach iterating
+	// over every possible starting gap sequence.
 	ans := 1
-	numAns := make([][]RangeAns, n)
-	// Now for each number in the original array, get the max possible answer.
-	// For each number, we analyse the number of gaps that are there in the
-	// original array and then see how many of them can be filled up to make
-	// the maximum consecutive sequence of that number.
-	for num := range numIndices {
-		// First, initialise the DP array for this number.
-		for i := 0; i < n; i++ {
-			numAns[i] = make([]RangeAns, n)
-			if arr[i] == num {
-				numAns[i][i] = RangeAns{1, 0}
-			} else {
-				if k > 0 {
-					numAns[i][i] = RangeAns{1, 1}
+	for num := range numIndices { // O(M)
+		indices := numIndices[num]
+		l := len(indices)
+
+		var currentGap int
+		var currentGapDelta int
+		for begin := range indices { // O(N)
+			numUsed := 0
+			total := 0
+			terminated := false
+			for i := begin; !terminated && i < l && numUsed <= k; i++ { // O(N)
+				// Consider the most of the current gap.
+				if i == 0 {
+					currentGap = indices[i]
+					total++ // For the one occurrence of num
+
+					if currentGap <= k-numUsed {
+						currentGapDelta = currentGap
+					} else {
+						currentGapDelta = k - numUsed
+						terminated = true
+					}
 				} else {
-					numAns[i][i] = RangeAns{0, 0}
+					currentGap = indices[i] - indices[i-1] - 1
+
+					if currentGap <= k-numUsed {
+						if total == 0 {
+							total += 2 // Since the edges have not been considered earlier.
+						} else {
+							total++ // Only the second edge will be added.
+						}
+						currentGapDelta = currentGap
+					} else {
+						if total == 0 {
+							total++
+						}
+						// The entire gap cannot be used. So we will use the
+						// max possible as we are being greedy.
+						currentGapDelta = k - numUsed
+						terminated = true
+					}
 				}
+				total += currentGapDelta
+				numUsed += currentGapDelta
+			} // for i
+
+			// Consider the trailing gap too.
+			if !terminated {
+				currentGap = n - 1 - indices[l-1]
+				if currentGap <= k-numUsed {
+					currentGapDelta = currentGap
+				} else {
+					currentGapDelta = k - numUsed
+					terminated = true
+				}
+				total += currentGapDelta
+				numUsed += currentGapDelta
 			}
-		}
-		// fmt.Printf("%d: %v\n", num, numAns)
 
-		// Now build the DP upwards using the following logic:
-		// numAns[p, q] = max(numAns[p, r].ans + numAns[r + 1, q].ans) for any
-		// r >= p and <= q, such that
-		// numAns[p, r].numKUsed + numAns[r + 1, q].numKUsed <= k
-		for l := 1; l < n; l++ {
-			for p := 0; p < n-l; p++ {
-				q := p + l
-				for r := p; r < q; r++ {
-					tmpAns := numAns[p][r].ans + numAns[r+1][q].ans
-					tmpKUsed := numAns[p][r].numKUsed + numAns[r+1][q].numKUsed
-
-					if tmpKUsed > k {
-						surplus := tmpKUsed - k
-						tmpAns -= surplus
-						tmpKUsed = k
-					}
-
-					if tmpAns < numAns[p][q].ans {
-						continue
-					}
-
-					if tmpAns == numAns[p][q].ans && tmpKUsed >= numAns[p][q].numKUsed {
-						continue
-					}
-
-					numAns[p][q].ans = tmpAns
-					numAns[p][q].numKUsed = tmpKUsed
-
-					ans = max(ans, tmpAns)
-				} // for r
-			} //for p
-		} // for l
-
-		// fmt.Printf("%d: %v\n", num, numAns)
-	} // for num
+			ans = max(ans, total)
+		} // for begin
+	} // for num - total O(M * N * N)
 
 	return ans
 }
@@ -105,6 +111,12 @@ func main() {
 		{[]int{1, 2, 2, 2}, 2},
 		{[]int{1, 2, 2, 2}, 0},
 		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2},
+		{[]int{1, 2, 3, 2, 3, 1}, 1},
+		{[]int{1}, 1},
+		{[]int{1, 2, 2, 1}, 1},
+		{[]int{1, 2, 3, 2, 1}, 1},
+		{[]int{1, 2, 3, 2, 1}, 2},
+		{[]int{1, 2, 3, 2, 1}, 3},
 	}
 
 	for _, v := range inp {
