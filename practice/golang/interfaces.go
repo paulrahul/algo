64c0fac155aa1fc10bc442fa57e94dd102e7c9f3
paulrahul <paul.rahul@gmail.com>
2021-12-12 14:23:32 +0100

    Some new findings.

--- practice/golang/interfaces.go
+++ practice/golang/interfaces.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type I interface {
-  foo() string
+	foo() string
+	// bar() string  // Leads to compile error as it forces the subtypes to implement this.
 }
 
 type MyInt int32
 type MyFloat float64
 
 func (i MyInt) foo() string {
-  // if i == nil  {
-  //   fmt.Println("MyInt value not set yet!")
-  //   return ""
-  // }
+	// if i == nil  {
+	//   fmt.Println("MyInt value not set yet!")
+	//   return ""
+	// }
 
-  return "Hey " + describe(i)
+	return "Hey " + describe(i)
 }
 
 func (f MyFloat) foo() string {
-  return "Hey " + describe(f)
+	return "Hey " + describe(f)
 }
 
 func describe(v I) string {
-  s := fmt.Sprintf("%T %v", v, v)
-  return s
+	s := fmt.Sprintf("%T %v", v, v)
+	return s
 }
 
-
 func main() {
-  var mi MyInt
-  fmt.Println(mi.foo())
+	var mi MyInt
+	fmt.Println(mi.foo())
 
-  var mf MyFloat
-  fmt.Println(mf.foo())
+	var mf MyFloat
+	fmt.Println(mf.foo())
 
-  var ei interface {}
-  ei = 12312
+	var ei interface{}
+	ei = 12312
 
-  v, ok := ei.(string)
-  fmt.Println(v, ok)
+	v, ok := ei.(string)
+	fmt.Println(v, ok)
 }
