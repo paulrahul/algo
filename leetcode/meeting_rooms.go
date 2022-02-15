66a3594e441f5e4c7346e14bc1889edf244cc162
paulrahul <paul.rahul@gmail.com>
2022-02-15 19:18:20 +0100

    Better solution - O(nLogn)

--- leetcode/meeting_rooms.go
+++ leetcode/meeting_rooms.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Meeting struct {
 	Start int
@@ -19,32 +22,58 @@ func swapMeeting(meeting1, meeting2 *Meeting) {
 	swapVal(&meeting1.End, &meeting2.End)
 }
 
+// func checkAvailability(meetings []Meeting) bool {
+// 	// Sort the meetings and bail out the moment you find a meeting overlapping.
+// 	n := len(meetings)
+
+// 	var start1 int
+// 	var start2 int
+// 	for i := 0; i < n; i++ {
+// 		start1 = meetings[i].Start
+// 		for j := i + 1; j < n; j++ {
+// 			start2 = meetings[j].Start
+
+// 			if start2 > start1 {
+// 				continue
+// 			}
+
+// 			if start2 == start1 && meetings[j].End >= meetings[i].End {
+// 				continue
+// 			}
+
+// 			// Swap the two meetings.
+// 			swapMeeting(&meetings[i], &meetings[j])
+// 		}
+// 	}
+
+// 	// fmt.Printf("After sorting: %v\n", meetings)
+
+// 	// Now iterate over the sorted list looking for any overlap.
+// 	for i := 1; i < n; i++ {
+// 		if meetings[i-1].End > meetings[i].Start {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
+
+type By []Meeting
+
+func (m By) Len() int      { return len(m) }
+func (m By) Swap(i, j int) { m[i], m[j] = m[j], m[i] }
+func (m By) Less(i, j int) bool {
+	return m[i].Start < m[j].Start ||
+		(m[i].Start == m[j].Start && m[i].End < m[j].End)
+}
+
 func checkAvailability(meetings []Meeting) bool {
 	// Sort the meetings and bail out the moment you find a meeting overlapping.
-	n := len(meetings)
-
-	var start1 int
-	var start2 int
-	for i := 0; i < n; i++ {
-		start1 = meetings[i].Start
-		for j := i + 1; j < n; j++ {
-			start2 = meetings[j].Start
-
-			if start2 > start1 {
-				continue
-			}
-
-			if start2 == start1 && meetings[j].End >= meetings[i].End {
-				continue
-			}
-
-			// Swap the two meetings.
-			swapMeeting(&meetings[i], &meetings[j])
-		}
-	}
+	sort.Sort(By(meetings))
 
 	// fmt.Printf("After sorting: %v\n", meetings)
 
+	n := len(meetings)
 	// Now iterate over the sorted list looking for any overlap.
 	for i := 1; i < n; i++ {
 		if meetings[i-1].End > meetings[i].Start {
